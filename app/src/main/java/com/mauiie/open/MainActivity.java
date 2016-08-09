50033b4bc8620ece0f3e4ead67a377905543c6c5
Mauiie <taijinglei@163.com>
2016-08-09 19:22:45 +0800

    +）添加测试演示Demo

--- app/src/main/java/com/mauiie/open/MainActivity.java
+++ app/src/main/java/com/mauiie/open/MainActivity.java
@@ -2,12 +2,20 @@ package com.mauiie.open;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.testCrash).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String testStr = null;
+        int length = testStr.length();
     }
 }
--- app/src/main/java/com/mauiie/open/app/MyApplication.java
+++ app/src/main/java/com/mauiie/open/app/MyApplication.java
@@ -2,6 +2,10 @@ package com.mauiie.open.app;
 
 import android.app.Application;
 
+import com.mauiie.aech.AECHConfiguration;
+import com.mauiie.aech.AECrashHelper;
+import com.mauiie.aech.utils.LogUtil;
+
 /**
  * Created by TaiJL on 2016/8/8.
  */
@@ -9,5 +13,18 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+//      AECrashHelper.initCrashHandler(this);
+
+        AECrashHelper.initCrashHandler(this, new AECHConfiguration.Builder()
+                .setSaveToLocal(true)
+                .setReportToServer(true)
+                .setReporter(new AECHConfiguration.IAECHReporter() {
+                    @Override
+                    public void report(Throwable ex) {
+                        LogUtil.d("向服务器发送报告由你自己根据项目的环境定制实现");
+                    }
+                })
+//              .setLocalFolderPath()//设置本地异常日志文件存储路径
+                .build());
     }
 }
